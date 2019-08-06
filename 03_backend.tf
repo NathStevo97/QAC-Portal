d97cc24ab838cff5656e3e58f9338b4163e57c57
henrychia12 <henry_chia21@hotmail.co.uk>
2019-08-06 12:17:40 +0000

    variable, provider, frontend, api and database created

---
+++ 03_backend.tf
@@ -0,0 +1,36 @@
+resource "google_compute_instance" "api" {
+	name = "${var.name}-${var.api}"
+	machine_type = "${var.machine_type}"
+	zone = "${var.zone}"
+	tags = ["${var.name}-${var.api}"]
+	boot_disk {
+		initialize_params {
+			image = "${var.image}"
+		}
+	}
+	network_interface {
+		network = "${var.network}"
+		access_config {
+			// Ephemeral IP
+		}
+	}
+	metadata = {
+	sshKeys = "${var.ssh_user}:${file("${var.public_key}")}"
+	}
+	connection {
+		type = "ssh"
+		user = "${var.ssh_user}"
+	host = "$(google_compute_instance.express.network_interface.0.access_config.0.nat_ip}"
+		private_key = "${file("${var.private_key}")}"
+	}
+	
+	provisioner "remote-exec" {
+		inline = [
+			 "echo test passed"
+			 ]
+	}
+	
+	provisioner "remote-exec" {
+		script = "${var.api-script}"
+	}
+}
